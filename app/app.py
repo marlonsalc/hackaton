5fedf9a9a931c05c8de02220bd0d325515b9cea0
marlonsalc <marloneduardosalc@gmail.com>
2023-05-27 19:08:59 -0600

    new version

--- app/app.py
+++ app/app.py
@@ -1,27 +1,39 @@
-from flask import Flask, render_template
-from urllib.request import urlopen
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import time
 
-app = Flask(__name__)
+# Configurar el estilo de la gráfica
+st.set_option('deprecation.showPyplotGlobalUse', False)
+plt.style.use("seaborn")
 
-@app.route("/")
-def index():
-    cursos = ['PHP', 'HTML', 'PYTHON']
-    data = {
-        "titulo": "index",
-        "bienvenida": "¡Saludos!",
-        "cursos": cursos, 
-        "numero_cursos": len(cursos)
-    }
-    return render_template("index.html", data=data)
+# Crear el dashboard
+st.title("Dashboard de Gasto de Agua")
 
+# Crear una lista vacía para almacenar los datos
+historial_gasto = []
 
-def get_data():
-    url = 'https://datos.monterrey.gob.mx/api/3/action/datastore_search?resource_id=826aa323-0d03-4839-9b9f-ab262680087c&limit=5'
-    with urlopen(url) as fileobj:
-        content = fileobj.read()
-        return content
+gasto_actual_container = st.empty()
 
-if __name__ == "__main__":
-    app.run(debug=True)
-    get_data()
+# Generar y mostrar los datos en tiempo real
+chart = st.line_chart(historial_gasto)
+
+# Simular la generación de datos en tiempo real
+while True:
+    # Generar dato de gasto de agua aleatorio
+    gasto_actual = np.random.normal(loc=30, scale=10)  # Media: 30, Desviación estándar: 10
+    gasto_actual = max(gasto_actual, 0)  # Asegurar que el gasto no sea negativo
     
+    # Agregar el dato al historial
+    historial_gasto.append(gasto_actual)
+    
+    # Actualizar la gráfica en tiempo real
+    chart.add_rows([historial_gasto[-1]])
+    
+    # Mostrar el último dato de gasto de agua
+    gasto_actual_container.subheader("Gasto de Agua Actual: {:.2f} l/s".format(gasto_actual))
+    
+    
+    # Esperar 1 segundo antes de generar el siguiente dato
+    time.sleep(1)
